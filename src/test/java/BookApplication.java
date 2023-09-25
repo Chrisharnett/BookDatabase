f3f7ce06bbe62757e1b405de1a8cfd87479b3af3
Bard of Code <harnettmusic@gmail.com>
2023-09-25 10:05:25 -0230

    Functioning Project

--- src/test/java/BookApplication.java
+++ src/test/java/BookApplication.java
@@ -1,16 +1,109 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  * @author saxDev
  * studentnumber 20188141
  **/
 public class BookApplication {
     public static void main(String[] args) {
-        //•	Create a Java class called “BookApplication” with a main method that prompts the user to select any of the following options:
-        //•	Print all the books from the database (showing the authors)
-        //•	Print all the authors from the database (showing the books)
-        //•	Add a book to the database for an existing author
-        //•	Add a new author
-        //•	Quit
-        //
-        //•	Note that the user should be able to continue making choices until they quit.
+
+        BookDatabaseManager bdm = new BookDatabaseManager();
+
+        System.out.println("Welcome to the book Database Manager.");
+
+        Scanner input = new Scanner(System.in);
+        char c;
+//        List<Book> books = bdm.getBookList();
+        do {
+            printMenu();
+            c = Character.toUpperCase(input.next().charAt(0));
+            if (c == 'A') {
+                System.out.println();
+                for (Book book : bdm.getBookList()){
+                    System.out.printf("Title: %s\nAuthor(s): ", book.getTitle());
+                    for (Author author: book.getAuthorList()){
+                        System.out.printf("%s, %s; ", author.getLastName(), author.getFirstName());
+                    }
+                    System.out.println();
+                    System.out.println();
+                }
+            } else if (c == 'B') {
+                System.out.println();
+                for(Author author: bdm.getAuthorList()){
+                    System.out.printf("Author: %s\nCurrent Title(s): \n", author.getLastName() + ", " + author.getFirstName());
+                    for (Book book: author.getBookList()){
+                        System.out.printf("%s\n", book.getTitle());
+                    }
+                    System.out.println();
+                }
+            } else if (c == 'C') {
+                addBook(bdm);
+            } else if (c == 'D') {
+                addAuthor(bdm);
+            }
+        } while (c != 'Q');
+        System.out.println("GoodBye!");
+    }
+
+    public static void printMenu() {
+        System.out.println("\nPlease make a selection:");
+        System.out.println("A - Print all books.");
+        System.out.println("B - Print all authors.");
+        System.out.println("C - Add a new book (for an existing author).");
+        System.out.println("D - Add a new author.");
+        System.out.println("Q - quit");
+    }
+
+    public static void addAuthor(BookDatabaseManager bdm){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Author first name: ");
+        String firstName = "";
+        while (firstName.equals("")){
+            firstName = scanner.next();
+        }
+        System.out.println("Author last name: ");
+        String lastName = "";
+        while (lastName.equals("")){
+            lastName = scanner.next();
+        }
+        bdm.addNewAuthor(firstName, lastName);
+    }
+    public static void addBook(BookDatabaseManager bdm){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Select an author");
+        for (int i=0; i < bdm.getAuthorList().size(); i++){
+            System.out.printf("%d. %s, %s\n", i+1, bdm.getAuthorList().get(i).getLastName(), bdm.getAuthorList().get(i).getFirstName());
+        }
+        int authorIndex = scanner.nextInt() - 1;
+        System.out.println("Enter book details");
+        String isbn = "";
+        while (isbn.equals("")){
+            System.out.println("ISBN: ");
+            isbn = scanner.next();
+        }
+        String title = "";
+        while (title.equals("")){
+            System.out.println("Title: ");
+            title = scanner.next();
+        }
+        int editionNumber = 0;
+        while(editionNumber <= 0){
+            try{
+                System.out.println("Edition Number: ");
+                editionNumber = scanner.nextInt();
+            }catch(InputMismatchException e){
+                System.out.println("Invalid integer, try again.");
+                editionNumber=0;
+            }
+
+        }
+        String copyright = "";
+        while (copyright.equals("")){
+            System.out.println("Copyright: ");
+            copyright = scanner.next();
+        }
+        Book book = new Book(isbn, title, editionNumber, copyright);
+        bdm.addNewBook(book, bdm.getAuthorList().get(authorIndex));
     }
 }
--- src/test/java/BookDatabaseManager.java
+++ src/test/java/BookDatabaseManager.java
@@ -1,3 +1,6 @@
+
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -5,13 +8,21 @@ import java.util.List;
  * studentnumber 20188141
  **/
 public class BookDatabaseManager {
-    private String DATABASE_URL;
-    private String SELECT_QUERY_AUTHOR;
+    final String DATABASE_URL = "jdbc:mariadb://localhost:3306/books";
+    final String USER = "root";
+    final String PASS = "root";
+
     private List<Book> bookList;
     private List<Author> authorList;
 
 // Constructor for Book DatabaseManager that takes no parameters.
     public BookDatabaseManager() {
+
+        try{
+            loadDatabase();
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
     }
 
     public List<Book> getBookList() {
@@ -21,19 +32,113 @@ public class BookDatabaseManager {
     public List<Author> getAuthorList() {
         return authorList;
     }
-    public void addNewBook(Book book){
+
+    public void addNewBook(Book book, Author author){
+        try(Connection conn = DriverManager.getConnection(DATABASE_URL, USER, PASS)){
+            PreparedStatement pstmt = conn.prepareStatement("INSERT INTO titles VALUES(?, ?, ?, ?)");
+            pstmt.setString(1, book.getISBN());
+            pstmt.setString(2, book.getTitle());
+            pstmt.setInt(3, book.getEdition());
+            pstmt.setString(4, book.getCopyright());
+            pstmt.execute();
+
+            pstmt = conn.prepareStatement("INSERT INTO authorISBN VALUES(?, ?)");
+            pstmt.setInt(1, author.getId());
+            pstmt.setString(2, book.getISBN());
+            pstmt.execute();
+            loadDatabase();
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+    public void  addNewAuthor(String firstName, String lastName){
+        try (Connection conn = DriverManager.getConnection(DATABASE_URL, USER, PASS)){
+            PreparedStatement pstmt = conn.prepareStatement("INSERT INTO authors VALUES(DEFAULT, ?, ?)");
+            pstmt.setString(1, firstName);
+            pstmt.setString(2, lastName);
+            pstmt.execute();
+            loadDatabase();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
 
     }
-    public void  addNewAuthor(Author author){
-
-    }
-    private void loadDatabase(){
-
+    private void loadDatabase() throws SQLException {
+        bookList = new ArrayList<>();
+        authorList = new ArrayList<>();
+        loadBooks();
+        loadAuthors();
+        String queryAuthor ="SELECT a.authorID, a.firstName, a.lastName " +
+                            "FROM authors a JOIN authorISBN i ON(a.authorID = i.authorID)" +
+                            "JOIN titles t using(isbn)" +
+                            "WHERE i.isbn = ?";
+        for (Book book : bookList) {
+            try (Connection conn = DriverManager.getConnection(DATABASE_URL, USER, PASS)){
+                PreparedStatement pstmt = conn.prepareStatement(queryAuthor);
+                pstmt.setString(1, book.getISBN());
+                ResultSet results = pstmt.executeQuery();
+                List<Author> bookAuthors = new ArrayList<>();
+                while (results.next()) {
+                    for (Author author: authorList){
+                        if (author.getId() == results.getInt("authorId")){
+                            bookAuthors.add(author);
+                        }
+                    }
+                }
+                book.setAuthorList(bookAuthors);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        String queryTitle = "SELECT t.title, t.isbn, t.editionNumber, t.copyright " +
+                            "FROM authors a JOIN authorISBN i ON(a.authorID = i.authorID)" +
+                            "JOIN titles t using(isbn)" +
+                            "WHERE i.authorId = ?";
+        for (Author author: authorList){
+            try (Connection conn = DriverManager.getConnection(DATABASE_URL, USER, PASS)){
+                PreparedStatement pstmt = conn.prepareStatement(queryTitle);
+                pstmt.setInt(1, author.getId());
+                ResultSet results = pstmt.executeQuery();
+                List<Book> authorsBooks = new ArrayList<>();
+                while (results.next()) {
+                    for (Book book: bookList){
+                        if (results.getString("title").equalsIgnoreCase(book.getTitle())){
+                            authorsBooks.add(book);
+                        }
+                    }
+                }
+                author.setBookList(authorsBooks);
+            } catch(SQLException e){
+                e.printStackTrace();
+            }
+        }
     }
     private void loadBooks(){
-
+        try(Connection conn = DriverManager.getConnection(DATABASE_URL, USER, PASS);
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM titles")
+        ) {
+            while (rs.next()) {
+                Book book = new Book(rs.getString("isbn"), rs.getString("title"),
+                        rs.getInt("editionNumber"), rs.getString("copyright"));
+                bookList.add(book);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
-    private void loadAuthor(){
-
+    private void loadAuthors(){
+        try(Connection conn = DriverManager.getConnection(DATABASE_URL, USER, PASS);
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM authors")
+        ) {
+            while (rs.next()) {
+                Author author = new Author(rs.getInt("authorid"), rs.getString("firstName"),
+                        rs.getString("lastName"));
+                authorList.add(author);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
